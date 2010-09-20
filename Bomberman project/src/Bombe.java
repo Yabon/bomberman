3c9894d13d5f011095f28ca938d03fedb8c61a1a
benjamin <foulon.benjamin2@gmail.com>
2010-09-20 12:00:30 +0200

    MAJ2

--- Bomberman project/src/Bombe.java
+++ Bomberman project/src/Bombe.java
@@ -1,20 +1,31 @@
 import java.util.Date;
 
 
-public class Bombe implements Case {
+public class Bombe implements Runnable{
 
+	int x,y;
 	int tailleFlamme;
-	long datePose = System.currentTimeMillis();
-	long dateExplosion = datePose+5000;
+	long dateExplosion = System.currentTimeMillis()+5000;
 	
-	
-	public boolean est_destructible() {
-			return true;
+	public Bombe(int x, int y, int tailleFlamme){
+		this.x = x;
+		this.y = y;
+		this.tailleFlamme = tailleFlamme;
 	}
-		
+	
 	public boolean est_traversable() {
 			return false;
 	}
 	
+	public void run() {
+		while(dateExplosion > System.currentTimeMillis() ){
+			
+		}
+		PlateauGraphique.explosion(tailleFlamme);
+	}
+	
+	
+	
+	//new Thread(Bombe).start();
 		
 }
