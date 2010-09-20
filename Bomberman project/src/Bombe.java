963c5016cd55b565b4d8ab39426e5850d93ceec2
benjamin <foulon.benjamin2@gmail.com>
2010-09-20 10:42:28 +0200

    ajout classe joueur

---
+++ Bomberman project/src/Bombe.java
@@ -0,0 +1,22 @@
+
+public class Bombe extends Case {
+
+	@Override
+	boolean est_destructible() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	boolean est_joueur() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	boolean est_traversable() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
