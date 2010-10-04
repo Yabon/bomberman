ef2cb4c8b9056274ecb05d798ad92cf7d63a5c7e
billietl <louis.billiet@etudiant.univ-lille1.fr>
2010-10-04 13:59:33 +0200

    le joueur se déplace sans encombres, voir README

--- Bomberman project/src/Joueur.java
+++ Bomberman project/src/Joueur.java
@@ -1,74 +1,31 @@
-public class Joueur implements Case{
+import ucigame.Image;
+import ucigame.Sprite;
 
-	public static enum Direction {N,S,E,O};
+public class Joueur extends Sprite {
+
+	public static enum Direction {
+		N, S, E, O
+	};
+
 	Direction dir;
 	int numero;
-	int hauteur,largeur;
-	
+	int hauteur, largeur;
+
+	public Joueur(Direction dir, int numero, int hauteur, int largeur, Image i) {
+		super(i);
+		this.dir = dir;
+		this.numero = numero;
+		this.hauteur = hauteur;
+		this.largeur = largeur;
+		this.position(largeur * 64, hauteur*48);
+	}
+
 	public int getHauteur() {
 		return hauteur;
 	}
 
 	public int getLargeur() {
 		return largeur;
-	}
-	
-	public void move(Direction d, boolean b){
-		this.dir = d;
-		switch(d){
-		case N:
-			if(b)hauteur++;
-			break;
-		case S:
-			if(b)hauteur--;
-			break;
-		case O:
-			if(b)largeur--;
-			break;
-		case E:
-			if(b)largeur++;
-			break;
-		}
-	}
-
-	public Joueur(Direction dir, int numero, int hauteur, int largeur){
-		this.dir = dir;
-		this.numero = numero;
-	}
-	
-	@Override
-	public boolean est_destructible() {
-		return true;
-	}
-
-	@Override
-	public boolean est_traversable() {
-		return false;
-	}
-	
-	public String toString(){
-		
-		switch (dir) {
-
-		case N :
-			return "NN";
-
-		case O :
-			return "OO";
-
-		case E :
-			return "EE";
-
-		case S :
-			return "SS";
-		}
-		
-		return " ";
-	}
-
-	@Override
-	public boolean est_joueur() {
-		return true;
 	}
 
 }
--- Bomberman project/src/Plateau.java
+++ Bomberman project/src/Plateau.java
@@ -3,173 +3,232 @@ import java.util.ArrayList;
 import ucigame.*;
 
 public class Plateau extends Ucigame {
-	
+
 	/**
-	 * On utilisera la première dimension pour la verticalité et la deuxième pour l'horizontalité
+	 * On utilisera la première dimension pour la verticalité et la deuxième
+	 * pour l'horizontalité
 	 */
 	private Case[][] grilleJeu;
-	List<Bombe> bombes;
-	public int ratio = 100;//Entre 0 et 100 represente le pourcentage de murs déstructibles
-	Joueur joueur1 = new Joueur(Joueur.Direction.N,1,1,1);
-	Sprite spriteJoueur1;
 	
-	private Plateau(int h, int l){
+	private List<Bombe> bombes;
+	public int ratio = 0;// Entre 0 et 100 represente le pourcentage de murs
+	// destructibles
+	private Joueur joueur1;
+
+	private Plateau(int h, int l) {
 		grilleJeu = new Case[h][l];
-		//generation du millieu du plateau
-		for(int hauteur = 1 ; hauteur < grilleJeu.length-1; hauteur++){
-			for(int largeur = 1 ; largeur < grilleJeu[0].length-1; largeur++){
-				if(Math.random()*100>ratio){
-					grilleJeu[hauteur][largeur] = new Chemin();	
-				}else{
-					grilleJeu[hauteur][largeur] = new Mur_Destructible();	
+		// generation du millieu du plateau
+		for (int hauteur = 1; hauteur < grilleJeu.length - 1; hauteur++) {
+			for (int largeur = 1; largeur < grilleJeu[0].length - 1; largeur++) {
+				if (Math.random() * 100 > ratio) {
+					grilleJeu[hauteur][largeur] = new Chemin(getImage(
+							"../images/bloc/pave/karodark.gif", 255, 255, 255));
+				} else {
+					grilleJeu[hauteur][largeur] = new Mur_Destructible(
+							getImage("../images/bloc/pave/cityfree.gif", 255,
+									255, 255));
 				}
 			}
 		}
-		//Generation du tour indéstructible
-		for(int hauteur = 0 ; hauteur < grilleJeu.length; hauteur++){
-			grilleJeu[hauteur][0] = new Mur_Indestructible();
-			grilleJeu[hauteur][14] = new Mur_Indestructible();
+		// Generation du tour indéstructible
+		for (int hauteur = 0; hauteur < grilleJeu.length; hauteur++) {
+			grilleJeu[hauteur][0] = new Mur_Indestructible(getImage(
+					"../images/bloc/pave/brick.gif", 255, 255, 255));
+			grilleJeu[hauteur][14] = new Mur_Indestructible(getImage(
+					"../images/bloc/pave/brick.gif", 255, 255, 255));
 		}
-		for(int largeur = 0 ; largeur < grilleJeu[0].length; largeur++){
-			grilleJeu[0][largeur] = new Mur_Indestructible();
-			grilleJeu[12][largeur] = new Mur_Indestructible();
+		for (int largeur = 0; largeur < grilleJeu[0].length; largeur++) {
+			grilleJeu[0][largeur] = new Mur_Indestructible(getImage(
+					"../images/bloc/pave/brick.gif", 255, 255, 255));
+			grilleJeu[12][largeur] = new Mur_Indestructible(getImage(
+					"../images/bloc/pave/brick.gif", 255, 255, 255));
 		}
-		
-		//Generation des cases indéstructibles au millieu du terrain
-		for(int hauteur = 2 ; hauteur < grilleJeu.length-1; hauteur+=2){
-			for(int largeur = 2 ; largeur < grilleJeu[0].length-1; largeur+=2){
-				grilleJeu[hauteur][largeur] = new Mur_Indestructible();
+
+		// Generation des cases indéstructibles au millieu du terrain
+		for (int hauteur = 2; hauteur < grilleJeu.length - 1; hauteur += 2) {
+			for (int largeur = 2; largeur < grilleJeu[0].length - 1; largeur += 2) {
+				grilleJeu[hauteur][largeur] = new Mur_Indestructible(getImage(
+						"../images/bloc/pave/brick.gif", 255, 255, 255));
 			}
 		}
-		bombes = new ArrayList<Bombe>(1024); // taille de la liste a l'origine pour éviter la réallocation
+		bombes = new ArrayList<Bombe>(1024); // taille de la liste a l'origine
+		// pour éviter la réallocation
 	}
-	
-	public Plateau(){
+
+	public Plateau() {
+
 		grilleJeu = new Case[13][15];
 		// à implémenter : génération d'un plateau aléatoire
 		genererTerrain();
-		//placer les joueurs a implémenter
-		
-		bombes = new ArrayList<Bombe>(1024); // taille de la liste a l'origine pour éviter la réallocation
+		// placer les joueurs a implémenter
+
+		bombes = new ArrayList<Bombe>(1024); // taille de la liste a l'origine
+		// pour éviter la réallocation
 	}
-	
-	public Case getCase(int posX, int posY){
+
+	public Case getCase(int posX, int posY) {
 		return grilleJeu[posY][posX];
 	}
-	
-	private void genererTerrain(){
 
-	
-		//generation du millieu du plateau
-		for(int hauteur = 1 ; hauteur < grilleJeu.length-1; hauteur++){
-			for(int largeur = 1 ; largeur < grilleJeu[0].length-1; largeur++){
-				if(Math.random()*100>ratio){
-					grilleJeu[hauteur][largeur] = new Chemin();	
-				}else{
-					grilleJeu[hauteur][largeur] = new Mur_Destructible();	
+	private void genererTerrain() {
+
+		// generation du millieu du plateau
+		for (int hauteur = 1; hauteur < grilleJeu.length - 1; hauteur++) {
+			for (int largeur = 1; largeur < grilleJeu[0].length - 1; largeur++) {
+				if (Math.random() * 100 > ratio) {
+					grilleJeu[hauteur][largeur] = new Chemin(getImage(
+							"../images/bloc/pave/karodark.gif", 255, 255, 255));
+				} else {
+					grilleJeu[hauteur][largeur] = new Mur_Destructible(
+							getImage("../images/bloc/pave/cityfree.gif", 255,
+									255, 255));
 				}
 			}
 		}
-		//Generation du tour indéstructible
-		for(int hauteur = 0 ; hauteur < grilleJeu.length; hauteur++){
-			grilleJeu[hauteur][0] = new Mur_Indestructible();
-			grilleJeu[hauteur][14] = new Mur_Indestructible();
+		// Generation du tour indéstructible
+		for (int hauteur = 0; hauteur < grilleJeu.length; hauteur++) {
+			grilleJeu[hauteur][0] = new Mur_Indestructible(getImage(
+					"../images/bloc/brique/brick.gif", 255, 255, 255));
+			grilleJeu[hauteur][14] = new Mur_Indestructible(getImage(
+					"../images/bloc/brique/brick.gif", 255, 255, 255));
 		}
-		for(int largeur = 0 ; largeur < grilleJeu[0].length; largeur++){
-			grilleJeu[0][largeur] = new Mur_Indestructible();
-			grilleJeu[12][largeur] = new Mur_Indestructible();
+		for (int largeur = 0; largeur < grilleJeu[0].length; largeur++) {
+			grilleJeu[0][largeur] = new Mur_Indestructible(getImage(
+					"../images/bloc/brique/brick.gif", 255, 255, 255));
+			grilleJeu[12][largeur] = new Mur_Indestructible(getImage(
+					"../images/bloc/brique/brick.gif", 255, 255, 255));
 		}
-		
-		//Generation des cases indéstructibles au millieu du terrain
-		for(int hauteur = 2 ; hauteur < grilleJeu.length-1; hauteur+=2){
-			for(int largeur = 2 ; largeur < grilleJeu[0].length-1; largeur+=2){
-				grilleJeu[hauteur][largeur] = new Mur_Indestructible();
+
+		// Generation des cases indéstructibles au millieu du terrain
+		for (int hauteur = 2; hauteur < grilleJeu.length - 1; hauteur += 2) {
+			for (int largeur = 2; largeur < grilleJeu[0].length - 1; largeur += 2) {
+				grilleJeu[hauteur][largeur] = new Mur_Indestructible(getImage(
+						"../images/bloc/brique/brick.gif", 255, 255, 255));
 			}
 		}
+
+		// Generation du depart des joueurs
+
 		
-		//Generation du depart des joueurs
+		grilleJeu[1][2] = new Chemin(getImage(
+				"../images/bloc/pave/karodark.gif", 255, 255, 255)); 
+		grilleJeu[2][1] = new Chemin(getImage(
+				"../images/bloc/pave/karodark.gif", 255, 255, 255));
+		grilleJeu[1][1] = new Chemin(getImage(
+				"../images/bloc/pave/karodark.gif", 255, 255, 255));
+
 		
-										/* ## */
-		grilleJeu[1][2] = new Chemin(); /* #  */
-		grilleJeu[2][1] = new Chemin();
-		//grilleJeu[1][1] = new Chemin();
-		grilleJeu[1][1] = joueur1;
-		
-		grilleJeu[11][13] = new Chemin();/*  # */
-		grilleJeu[11][12] = new Chemin();/* ## */
-		grilleJeu[10][13] = new Chemin();
-		
-		grilleJeu[1][13] = new Chemin();/* ## */
-		grilleJeu[1][12] = new Chemin();/*  # */
-		grilleJeu[2][13] = new Chemin();
-		
-		grilleJeu[11][1] = new Chemin();/* #  */
-		grilleJeu[11][2] = new Chemin();/* ## */
-		grilleJeu[10][1] = new Chemin();
-		
+
+		grilleJeu[11][13] = new Chemin(getImage(
+				"../images/bloc/pave/karodark.gif", 255, 255, 255));/* # */
+		grilleJeu[11][12] = new Chemin(getImage(
+				"../images/bloc/pave/karodark.gif", 255, 255, 255));/* ## */
+		grilleJeu[10][13] = new Chemin(getImage(
+				"../images/bloc/pave/karodark.gif", 255, 255, 255));
+
+		grilleJeu[1][13] = new Chemin(getImage(
+				"../images/bloc/pave/karodark.gif", 255, 255, 255));/* ## */
+		grilleJeu[1][12] = new Chemin(getImage(
+				"../images/bloc/pave/karodark.gif", 255, 255, 255));/* # */
+		grilleJeu[2][13] = new Chemin(getImage(
+				"../images/bloc/pave/karodark.gif", 255, 255, 255));
+
+		grilleJeu[11][1] = new Chemin(getImage(
+				"../images/bloc/pave/karodark.gif", 255, 255, 255));/* # */
+		grilleJeu[11][2] = new Chemin(getImage(
+				"../images/bloc/pave/karodark.gif", 255, 255, 255));/* ## */
+		grilleJeu[10][1] = new Chemin(getImage(
+				"../images/bloc/pave/karodark.gif", 255, 255, 255));
+
 	}
 
-	public void afficherASCII(){
-		for(int hauteur = 0 ; hauteur < grilleJeu.length; hauteur++){
-			for(int largeur = 0 ; largeur < grilleJeu[0].length; largeur++){
-				if(hauteur == joueur1.hauteur && largeur == joueur1.largeur){
-					System.out.println(joueur1.toString());					
-				}else if(grilleJeu[hauteur][largeur].est_traversable()){
-					System.out.print("  ");
-				}else if (grilleJeu[hauteur][largeur].est_destructible()){
-					System.out.print("dd");
-				}else{
-					System.out.print("[]");
+	public void setup() {
+		/* AJOUT TEST */
+		// génération d'un plateau aléatoire
+		genererTerrain();
+		genererJoueurs();
+
+		
+		bombes = new ArrayList<Bombe>(1024); // taille de la liste a l'origine
+		// pour éviter la réallocation
+		/* FIN AJOUT TEST */
+
+		resize(13 * 64, 15 * 48);
+		window.size(13 * 64, 15 * 48);
+		window.title("IBomberMan");
+		window.showFPS();
+		canvas.background(100, 255, 100);
+		this.resize(13 * 64, 15 * 48);
+
+		for (int hauteur = 0; hauteur < grilleJeu.length; hauteur++) {
+			for (int largeur = 0; largeur < grilleJeu[0].length; largeur++) {
+					grilleJeu[hauteur][largeur].position(hauteur * 64, largeur * 48);
+			}
+		}
+
+		framerate(30);
+
+	}
+
+	private void genererJoueurs() {
+		joueur1 = new Joueur(Joueur.Direction.N, 1, 5 ,7 , getImage(
+				"../images/joueur/skelbas.gif", 255, 255, 255));
+	}
+
+	public void move() {
+		for(int i=0; i<grilleJeu.length; i++){
+			for(int j=0; j<grilleJeu[0].length; j++){
+				if (!(grilleJeu[i][j] instanceof Chemin)){
+					joueur1.stopIfCollidesWith(grilleJeu[i][j]);
 				}
 			}
-			System.out.println("");
 		}
-	}
-	
-	public void setup(){
-		window.size(250, 250);
-        window.title("IBomberMan");
-        window.showFPS();
-        canvas.background(255, 200, 200);
-
-        spriteJoueur1 = makeSprite(getImage("../images/joueur/joueur_bleu/bbas1.gif", 255, 255, 255));
-        
-        spriteJoueur1.position(joueur1.getHauteur(), joueur1.getLargeur());
-
-        framerate(30);
-
-	}
-	
-	public void move(){
 		if(keyboard.isDown(keyboard.Z)){
-			spriteJoueur1.nextY(spriteJoueur1.y()-10);
-			joueur1.move(Joueur.Direction.N, grilleJeu[joueur1.getHauteur()+1][joueur1.getLargeur()].est_traversable());
-		}else if(keyboard.isDown(keyboard.S)){
-			spriteJoueur1.nextY(spriteJoueur1.y()+10);
-			joueur1.move(Joueur.Direction.S, grilleJeu[joueur1.getHauteur()-1][joueur1.getLargeur()].est_traversable());
-		}else if(keyboard.isDown(keyboard.Q)){
-			spriteJoueur1.nextX(spriteJoueur1.x()-10);
-			joueur1.move(Joueur.Direction.O, grilleJeu[joueur1.getHauteur()][joueur1.getLargeur()-1].est_traversable());
-		}else if(keyboard.isDown(keyboard.D)){
-			spriteJoueur1.nextX(spriteJoueur1.x()+10);
-			joueur1.move(Joueur.Direction.E, grilleJeu[joueur1.getHauteur()][joueur1.getLargeur()+1].est_traversable());
+			joueur1.nextY(joueur1.y()-10);
+		}
+		if(keyboard.isDown(keyboard.S)){
+			joueur1.nextY(joueur1.y()+10);
+		}
+		if(keyboard.isDown(keyboard.Q)){
+			joueur1.nextX(joueur1.x()-10);
+		}
+		if(keyboard.isDown(keyboard.D)){
+			joueur1.nextX(joueur1.x()+10);
 		}
 	}
-	
-	public void draw(){
+
+	public void draw() {
+		this.move();
+		
 		canvas.clear();
-
-        this.move();
-
-        spriteJoueur1.draw();
+				
+		for (int hauteur = 0; hauteur < grilleJeu.length; hauteur++) {
+			for (int largeur = 0; largeur < grilleJeu[0].length; largeur++) {
+				grilleJeu[hauteur][largeur].draw();		
+			}
+		}
+		
+		joueur1.draw();
 	}
-	
-	public static void main (String args[]){
-		Plateau p = new Plateau(20, 20);
-		p.afficherASCII();
-	}
-	
+
+	public void onKeyPress()
+    {
+            if (keyboard.isDown(keyboard.UP, keyboard.W))
+            {
+                joueur1.nextY(joueur1.y() - 10);
+            }
+            else if (keyboard.isDown(keyboard.DOWN, keyboard.S))
+            {
+                joueur1.nextY(joueur1.y() + 10);
+            }
+            else if (keyboard.isDown(keyboard.LEFT, keyboard.A))
+            {
+                joueur1.nextX(joueur1.x() - 10);
+            }
+            else if (keyboard.isDown(keyboard.RIGHT, keyboard.D))
+            {
+                joueur1.nextX(joueur1.x() + 10);
+            }
+        
+    }
 }
-
-
-
